1cdc0a04a7961b600b6f2974ca12397ff6eca263
syki66 <syeonki66@gmail.com>
2020-02-13 15:25:13 +0900

    Updates

---
+++ precache-manifest.f1bd40a6db95632b763a19bf4c149511.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b816ec35eaea1b8792c28d5ca772971",
+    "url": "/JS-react--official_suvinCos_webpage/index.html"
+  },
+  {
+    "revision": "187848fb1903e3e702ad",
+    "url": "/JS-react--official_suvinCos_webpage/static/css/2.13751919.chunk.css"
+  },
+  {
+    "revision": "7325627d0f741b225313",
+    "url": "/JS-react--official_suvinCos_webpage/static/css/main.52564a62.chunk.css"
+  },
+  {
+    "revision": "187848fb1903e3e702ad",
+    "url": "/JS-react--official_suvinCos_webpage/static/js/2.b1813865.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/JS-react--official_suvinCos_webpage/static/js/2.b1813865.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7325627d0f741b225313",
+    "url": "/JS-react--official_suvinCos_webpage/static/js/main.72b032ef.chunk.js"
+  },
+  {
+    "revision": "ccf29202f1de7faafb5e",
+    "url": "/JS-react--official_suvinCos_webpage/static/js/runtime-main.85fbc60e.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/JS-react--official_suvinCos_webpage/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/JS-react--official_suvinCos_webpage/static/media/arrow_right.8ef6a08c.svg"
+  }
+]);
