dc369a8f54d29e018465d5419031c60cf99e5cfa
syki66 <syeonki66@gmail.com>
2020-05-18 02:04:55 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.efe16b4a85b68128a13c968113a1f024.js"
+  "/precache-manifest.d903da4830e8070f71ab33b28c99ace5.js"
 );
 
 self.addEventListener('message', (event) => {
